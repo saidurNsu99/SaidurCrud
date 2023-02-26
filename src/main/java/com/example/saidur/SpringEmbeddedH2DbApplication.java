4d9e92a60269a6a5b2dfb5b64492cdf8478d9067
Md. Saidur Rahman <37741326+saidurNsu99@users.noreply.github.com>
2023-02-26 16:14:40 +0600

    Add files via upload

--- src/main/java/com/example/saidur/SpringEmbeddedH2DbApplication.java
+++ src/main/java/com/example/saidur/SpringEmbeddedH2DbApplication.java
@@ -1,13 +1,23 @@
 package com.example.saidur;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SpringEmbeddedH2DbApplication {
-
+	@Autowired
+	private ObjectMapper objectMapper;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmbeddedH2DbApplication.class, args);
 	}
 
+	@PostConstruct
+	public void setUp() {
+		objectMapper.registerModule(new JavaTimeModule());
+	}
 }
